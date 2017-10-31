4123af56cf2b7c1087aa87557aa50bf229da62a6
muffintrap <muffintrap@protonmail.com>
2017-10-31 22:09:01 +0200

    Loads normals from OBJ. Limits mipmaps to level 0.

--- data/shaders/fragment.fs
+++ data/shaders/fragment.fs
@@ -2,6 +2,7 @@
 
 uniform sampler2D inputTexture;
 
+in vec3 fNormal;
 in vec2 fTexCoord;
 in vec4 fDiffuseColor; // must match name in vertex shader
 out vec4 fragColor; // first out variable is automatically written to the screen
--- source/Scene.cs
+++ source/Scene.cs
@@ -44,22 +44,23 @@ class Scene
 
 		shaderProgram.Use();
 		Mesh.PositionDataIndex = shaderProgram.GetAttributeLocation("vPosition");
+		Mesh.NormalDataIndex = shaderProgram.GetAttributeLocation("vNormal");
 		Mesh.TexCoordDataIndex = shaderProgram.GetAttributeLocation("vTexCoord");
 		Mesh.ColorDataIndex = shaderProgram.GetUniformLocation("uDiffuseColor");
 		Mesh.ScaleDataIndex = shaderProgram.GetUniformLocation("uScale");
 		shaderProgram.setSamplerUniform("inputTexture", 0);
 
-		origoTriangle = addMesh(Mesh.CreateTriangleMesh(), new Vector3(0, 0, 0), new Color4(1.0f, 1.0f, 1.0f, 1.0f), 0.1f);
-		xTriangle = addMesh(Mesh.CreateTriangleMesh(), new Vector3(1, 0, 0), new Color4(1, 0.2f, 0.2f, 1), 0.1f);
-		zTriangle = addMesh(Mesh.CreateTriangleMesh(), new Vector3(0, 0, 1), new Color4(0.2f, 1, 0.2f, 1), 0.1f);
-		yTriangle = addMesh(Mesh.CreateTriangleMesh(), new Vector3(0, 1, 0), new Color4(0.2f, 0.2f, 1, 1), 0.1f);
+		//origoTriangle = addMesh(Mesh.CreateTriangleMesh(), new Vector3(0, 0, 0), new Color4(1.0f, 1.0f, 1.0f, 1.0f), 0.1f);
+		//xTriangle = addMesh(Mesh.CreateTriangleMesh(), new Vector3(1, 0, 0), new Color4(1, 0.2f, 0.2f, 1), 0.1f);
+		//zTriangle = addMesh(Mesh.CreateTriangleMesh(), new Vector3(0, 0, 1), new Color4(0.2f, 1, 0.2f, 1), 0.1f);
+		//yTriangle = addMesh(Mesh.CreateTriangleMesh(), new Vector3(0, 1, 0), new Color4(0.2f, 0.2f, 1, 1), 0.1f);
 
-		voxelMesh = addMesh(Mesh.CreateFromFile("../data/models/monu9/monu9.obj"), new Vector3(0.0f, 0.0f, 0.0f), new Color4(1.0f, 1.0f, 1.0f, 1.0f), 0.1f);
+		voxelMesh = addMesh(Mesh.CreateFromFile("../data/models/monu9.obj"), new Vector3(0.0f, 0.0f, 0.0f), new Color4(1.0f, 1.0f, 1.0f, 1.0f), 0.1f);
 
 		projectionMatrix = new Matrix4Uniform("projectionMatrix");
 		projectionMatrix.Matrix = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver2, 16.0f / 9.0f, 0.1f, 100f);
 
-		cameraPosition = new Vector3(0, 0, -2);
+		cameraPosition = new Vector3(0, -5, -10);
 		cameraDirection = new Vector3(0, 0, 1.0f);
 		sceneUp = new Vector3(0.0f, 1.0f, 0.0f);
 
@@ -99,11 +100,11 @@ class Scene
 		projectionMatrix.Set(shaderProgram);
 		viewMatrix.Set(shaderProgram);
 
-		drawMesh(zTriangle);
-		drawMesh(origoTriangle);
-		drawMesh(xTriangle);
-		
-		drawMesh(yTriangle);
+		//drawMesh(zTriangle);
+		//drawMesh(origoTriangle);
+		//drawMesh(xTriangle);
+		//
+		//drawMesh(yTriangle);
 		//
 		drawMesh(voxelMesh);
 
