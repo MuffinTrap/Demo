7041f5a38adebda924991f874d247d93911dd28b
muffintrap <muffintrap@hotmail.com>
2017-11-04 16:33:24 +0200

    New try with music playback

--- source/MainWindow.cs
+++ source/MainWindow.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
 
 using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
 using OpenTK.Input;
+
+using OpenTK.Audio.OpenAL;
 
 using RocketNet;
 
@@ -20,6 +24,17 @@ namespace OpenTkConsole
 			while ( GL.GetError() != ErrorCode.NoError)
 			{
 				Console.WriteLine("GL error in " + place);
+				errorFound = true;
+			}
+			return errorFound;
+		}
+
+		static public bool checkALError(string place)
+		{
+			bool errorFound = false;
+			while ( AL.GetError() != ALError.NoError)
+			{
+				Console.WriteLine("AL error in " + place);
 				errorFound = true;
 			}
 			return errorFound;
@@ -46,7 +61,7 @@ namespace OpenTkConsole
 
         bool useSync;
 
-		Scene testScene;
+		IScene testScene;
         Stopwatch timer;
 
         public MainWindow()
@@ -75,30 +90,40 @@ namespace OpenTkConsole
             running = true;
 			paused = true;
 			spaceDown = false;
-			
+
+
+
+
+			// Audio
+			initAudio();
+
+
+
 			// SYNC
 			useSync = false;
-			// /// ////////////////////
-
 			loadSyncer();
+			bpm = 120;
+			rowsPerBeat = 4;
+			songLength = 5.0f; // seconds
 
+
+			// Materials and scenes
+			// Pass syncer to scenes.
 			try
 			{
 				MaterialManager.init("../data/materials/");
 
-				testScene = new Scene();
+				testScene = new EmptyScene();
 				testScene.loadScene();
 			}
 			catch (Exception exception)
 			{
 				Console.WriteLine("Caugh exception when loading scene" + exception.Message);
 			}
-            bpm = 120;
-            rowsPerBeat = 4;
-            songLength = 5.0f; // seconds
 
-            timer = new Stopwatch();
 
+			// Timing
+			timer = new Stopwatch();
             timer.Start();
         }
 
@@ -179,8 +204,9 @@ namespace OpenTkConsole
             {
 				running = false;
                 timer.Stop();
-                syncDevice.Dispose();
-                Exit();
+
+				cleanupAndExit();
+               
             }
 
 			// Pass input to scene
@@ -250,6 +276,111 @@ namespace OpenTkConsole
 				Console.WriteLine("Socket exception: " + socketE.Message);
 				running = false;
 			}
+		}
+
+		void initAudio()
+		{
+			
+
+			
+
+			IntPtr nullDevice = System.IntPtr.Zero;
+			IList<string> allDevices = Alc.GetString(nullDevice, AlcGetStringList.DeviceSpecifier);
+			foreach(string s in allDevices)
+			{
+				Console.WriteLine("OpenAL device " + s);
+			}
+
+			// Open preferred device
+			ContextHandle alContext;
+			IntPtr ALDevicePtr = Alc.OpenDevice(null);
+			if (ALDevicePtr != null)
+			{
+				int[] deviceAttributes = null;
+				alContext = Alc.CreateContext(ALDevicePtr, deviceAttributes);
+				Alc.MakeContextCurrent(alContext);
+			}
+			else
+			{
+				Console.WriteLine("Could not get AL device");
+				return;
+			}
+
+			string alRenderer = AL.Get(ALGetString.Renderer);
+			string alVendor = AL.Get(ALGetString.Vendor);
+			string alVersion = AL.Get(ALGetString.Version);
+
+			Console.WriteLine("OpenAL Renderer {0}  Vendor {1}  Version {2}", alRenderer, alVendor, alVersion);
+
+
+			Error.checkALError("initAudio");
+			int alBuffer = AL.GenBuffer();
+			Error.checkALError("initAudio genBuffer");
+
+			
+			int frequenzy = 44100;
+
+			// Buffer data
+			bool dataisVorbis = false;
+
+			string vorbisEXTName = "AL_EXT_vorbis";
+			if (AL.IsExtensionPresent(vorbisEXTName) && dataisVorbis)
+			{
+				Console.WriteLine("AL can use vorbis");
+				IntPtr vorbisBuffer = System.IntPtr.Zero;
+				int vorbisSize = 0;
+				AL.BufferData(alBuffer, ALFormat.VorbisExt, vorbisBuffer, vorbisSize, frequenzy);
+			}
+			else
+			{
+				// Load wav
+				
+				FileStream audioFile = File.Open("../data/music/bosca.wav", FileMode.Open, FileAccess.Read);
+				long wavSize = audioFile.Length;
+				byte[] audioContents = new byte[wavSize];
+
+				audioFile.Read(audioContents, 0, (int)wavSize);
+				IntPtr wavBuffer = Marshal.AllocHGlobal(audioContents.Length);
+				Marshal.Copy(audioContents, 0, wavBuffer, audioContents.Length);
+
+				AL.BufferData(alBuffer, ALFormat.Stereo16, wavBuffer, (int)wavSize, frequenzy);
+				Marshal.FreeHGlobal(wavBuffer);
+				audioFile.Close();
+			}
+
+			Error.checkALError("initAudio bufferAudio");
+
+			int alSource = AL.GenSource();
+			Error.checkALError("initAudio genSource");
+
+			// Attach buffer to source.
+			AL.Source(alSource, ALSourcei.Buffer, alBuffer);
+
+			// Set listener and source to same place
+			Vector3 listenerPos = new Vector3(0, 0, 0);
+			Vector3 sourcePos = new Vector3(0, 0, 0);
+			AL.Listener(ALListener3f.Position, ref listenerPos);
+			AL.Source(alSource, ALSource3f.Position, ref sourcePos);
+
+			// Play buffer
+			AL.SourcePlay(alSource);
+		}
+
+		void shutDownAudio()
+		{
+			ContextHandle alContext = Alc.GetCurrentContext();
+			IntPtr alDevice = Alc.GetContextsDevice(alContext);
+			ContextHandle emptyContext = ContextHandle.Zero;
+			Alc.MakeContextCurrent(emptyContext);
+			Alc.DestroyContext(alContext);
+			Alc.CloseDevice(alDevice);
+		}
+
+		void cleanupAndExit()
+		{
+			syncDevice.Dispose();
+			shutDownAudio();
+			Exit();
 		}
     }
 
